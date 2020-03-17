393ebdf95ae5f19d1c2b39246838d9aec45793f1
Luana Duarte <lua.duarte04@gmail.com>
2020-03-17 02:58:59 +0000

    adding url shortening part 1

--- express_server.js
+++ express_server.js
@@ -1,8 +1,15 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,10 +21,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  let randomString = "";
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+  for (let i = 0; i < 6; i++ ) {
+    randomString += characters.charAt(Math.floor(Math.random() * 62));
+  }
+  return randomString;
+}
+console.log(generateRandomString(6));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
