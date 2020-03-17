71771ebe23183561411f2fe560755c2293432c30
Luana Duarte <lua.duarte04@gmail.com>
2020-03-17 18:02:12 +0000

    tiny app short url part 2

--- express_server.js
+++ express_server.js
@@ -6,30 +6,40 @@ const PORT = 8080; // default port 8080
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// SAVE, STORE AND ASSIGN SMALL URL TO LONG URL
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`)
+
+  console.log(urlDatabase);
 });
 
+// "DATABASE"
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// RENDER THE PAGE FOR THE LIST OF SAVED URLS
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// RENDER THE PAGE FOR THE NEW URL FORM
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// RENDER THE PAGE FOR THE BRAND NEW GENERATED SHORT URL
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+// GENERATES SHORT URLS
 function generateRandomString() {
   let randomString = "";
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -39,27 +49,10 @@ function generateRandomString() {
   }
   return randomString;
 }
-console.log(generateRandomString(6));
 
+// SHOW ON CONSOLE THAT WE CONNECT TO THE SERVER
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
-
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
- 
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
+console.log(urlDatabase);
